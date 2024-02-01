0900e99141576944e7706a4d31fa0f8b16bacf13
byrax15 <byraxis@gmail.com>
2024-02-01 10:17:49 -0500

    replaced macro with static assert

--- cuda_rasterizer/CullOperator.h
+++ cuda_rasterizer/CullOperator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <array>
+#include <numeric>
 #include <type_traits>
 #include "cuda_runtime.h"
 
@@ -11,16 +12,18 @@ namespace FORWARD {
 		template<typename TComponent>
 		using vec_component = std::is_same<typename std::decay<TComponent>::type, float>;
 
-#define assert_vec(VecLike) \
-			static_assert(vec_component<decltype(VecLike::x)>::value \
-				&& vec_component<decltype(VecLike::y)>::value \
-				&& vec_component<decltype(VecLike::z)>::value, \
-				"VecLike requires float x,y,z members")
+		template <typename VecLike>
+		constexpr inline void is_vec() {
+			static_assert(vec_component<decltype(VecLike::x)>::value
+				&& vec_component<decltype(VecLike::y)>::value
+				&& vec_component<decltype(VecLike::z)>::value,
+				"VecLike requires float x,y,z members");
+		}
 
 		template<typename VecLike>
 		__host__ __device__ inline constexpr auto MakeInsideBoxLambda(VecLike p_orig)
 		{
-			assert_vec(VecLike);
+			is_vec<VecLike>();
 			return [p_orig](VecLike min, VecLike max) {
 				return !(p_orig.x < min.x || p_orig.y < min.y || p_orig.z < min.z ||
 					p_orig.x > max.x || p_orig.y > max.y || p_orig.z > max.z);
@@ -32,11 +35,11 @@ namespace FORWARD {
 			VecLike const* const boxmin, VecLike const* const boxmax, int boxcount,
 			Operator op, decltype(MakeInsideBoxLambda<VecLike>(VecLike{})) inside_box)
 		{
-			assert_vec(VecLike);
+			is_vec<VecLike>();
 
 			if (boxcount == 0)
 				return false;
-
+			
 			bool inside = inside_box(boxmin[0], boxmax[0]);
 			switch (op) {
 			case Operator::AND:
